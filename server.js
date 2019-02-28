a5ecd77ba719d562b2c2a942eea97a901eb1aae7
zlfzlfgmlwns <zlfzlfgmlwns@naver.com>
2019-02-28 22:59:51 +0900

    삭제 팝업창구현

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.get('/api/customers',(req,res)=>{
     connection.query(
       "SELECT * FROM CUSTOMER WHERE isDeleted = 0", 
       (err,rows, fields) => {
-        console.log(rows)
         res.send(rows);
       }
     )
